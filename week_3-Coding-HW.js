42dc239b4896e0fb011c3ede28f6ce6a31cdecd7
Jesus Morales <jesusmorales141512@gmail.com>
2022-08-23 20:31:22 -0500

    third attempt 8-23-22

--- week_3-Coding-HW.js
+++ week_3-Coding-HW.js
@@ -13,4 +13,47 @@ ages.push(8,55);
 console.log(ages[ages.length - 1] - ages[0]);
 
 //c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+totalAges = 0
+
+for (let i = 0; i < ages.length; i++) {
+    totalAges = totalAges + ages[i]
+    averageAge = totalAges / ages.length
+}
+
+console.log(averageAge);
+
+//2.	Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
+
+const names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+
+//a.	Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.
+ const namesLetters = [3, 5, 3, 5, 4, 3]
+
+ totalNamesLetters = 0
+
+ for (let i = 0; i < namesLetters.length; i++) {
+    totalNamesLetters = totalNamesLetters + namesLetters[i]
+    averageNamesLetters = totalNamesLetters / namesLetters.length
+ }
+
+ console.log(averageNamesLetters);
+
+ //b.	Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names.join(' '));
+}
+
+//3.	How do you access the last element of any array?
+
+console.log(ages[ages.length - 1]);
+console.log(names[names.length - 1]);
+
+//4.	How do you access the first element of any array?
+
+console.log(ages[0]);
+console.log(names[0]);
+
+//5.	Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
+
 
